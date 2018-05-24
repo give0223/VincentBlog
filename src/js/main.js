cab1857d6ee156f4d8ae92be9b44b8ffbba43912
Vincent_Lin <give0223@gmail.com>
2018-05-25 00:59:18 +0800

    navbar fixed of top

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,18 @@ $('.nav_link').click(function() {
   }, 700);
   return false;  
 });
+
+var navbar = document.querySelector('nav');
+var stickys = navbar.offsetTop;
+
+function barscroll() {
+  if(window.pageYOffset > stickys) {
+    navbar.classList.add('sticky');
+  }else {
+    navbar.classList.remove('sticky');
+  }
+}
+
+window.onscroll = function() {
+  barscroll();
+};
